3fdb111c60cb767e383e71297506985ec2d8bc0a
Giovani Ornellas <111402558+Giovanni-Ornellas@users.noreply.github.com>
2022-09-24 02:32:53 -0300

    Exercício 1003 do Beecrowd

---
+++ Iniciante/1003_SomaSimples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int A,B,SOMA;
+
+    //Recebe os valores de A e B
+    std::cin >> A >> B;
+
+    //Faz o cálculo da soma
+    SOMA = A+B;
+
+    //Imprime o valor da soma 
+    std::cout << "SOMA = " << SOMA << std::endl;
+
+    return 0;
+}
